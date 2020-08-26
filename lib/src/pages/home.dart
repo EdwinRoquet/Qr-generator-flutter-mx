57a74f11a0112b471220ea3e56bf86752a31aee1
EdwinRoquet <eroquetf@gmail.com>
2020-08-26 08:54:54 -0500

    segundo commit

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -5,21 +5,19 @@ import 'dart:ui';
 import 'package:flutter/rendering.dart';
 
 class GenerateScreen extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => GenerateScreenState();
 }
 
 class GenerateScreenState extends State<GenerateScreen> {
-
   static const double _topSectionTopPadding = 50.0;
   static const double _topSectionBottomPadding = 20.0;
   static const double _topSectionHeight = 50.0;
 
   GlobalKey globalKey = new GlobalKey();
-  String _dataString = "Hola escribe aqui tu codigo Qr";
+  String _dataString;
   String _inputErrorText;
-  final TextEditingController _textController =  TextEditingController();
+  final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +27,7 @@ class GenerateScreenState extends State<GenerateScreen> {
         title: Text('QR Code Generator'),
       ),
       body: GestureDetector(
-        onTap: (){
+        onTap: () {
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: _contentWidget(),
@@ -37,12 +35,12 @@ class GenerateScreenState extends State<GenerateScreen> {
     );
   }
 
-
   _contentWidget() {
-    final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
-    return  Container(
+    final bodyHeight = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).viewInsets.bottom;
+    return Container(
       color: const Color(0xFFFFFFFF),
-      child:  Column(
+      child: Column(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(
@@ -51,16 +49,16 @@ class GenerateScreenState extends State<GenerateScreen> {
               right: 10.0,
               bottom: _topSectionBottomPadding,
             ),
-            child:  Container(
+            child: Container(
               height: _topSectionHeight,
-              child:  Row(
+              child: Row(
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   Expanded(
-                    child:  TextField(
+                    child: TextField(
                       controller: _textController,
-                      decoration:  InputDecoration(
+                      decoration: InputDecoration(
                         hintText: "Escribe el codigo",
                         errorText: _inputErrorText,
                       ),
@@ -68,25 +66,29 @@ class GenerateScreenState extends State<GenerateScreen> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 10.0),
-                    child:  FlatButton(
+                    child: FlatButton(
                       textColor: Colors.white,
-                      child:Container(
+                      child: Container(
                         width: 100,
                         height: 50,
-                        decoration:  BoxDecoration(
+                        decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(
-                          colors: <Color>[
-                            Color(0xFF0D47A1),
-                            Color(0xFF1976D2),
-                            Color(0xFF42A5F5),
-                          ],
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
+                          ),
                         ),
-                      ),
-                        child:Center(child: Text("Generar", style: TextStyle(fontSize: 20),)) ,
+                        child: Center(
+                            child: Text(
+                          "Generar",
+                          style: TextStyle(fontSize: 20),
+                        )),
                       ),
                       onPressed: () {
-                        setState((){
+                        setState(() {
                           _dataString = _textController.text;
                           _inputErrorText = null;
                         });
@@ -98,14 +100,17 @@ class GenerateScreenState extends State<GenerateScreen> {
             ),
           ),
           Expanded(
-            child:  Center(
-              child: RepaintBoundary(
-                key: globalKey,
-                child: QrImage(
-                  data: 'https://intecmedia.com.mx/app-fergie/verificacion.php?codigo=$_dataString',
-                  size: 0.5 * bodyHeight,          
-                ),
-              ),
+            child: Center(
+              child: (_dataString == null)
+                  ? Container()
+                  : RepaintBoundary(
+                      key: globalKey,
+                      child: QrImage(
+                        data:
+                            'https://intecmedia.com.mx/app-fergie/verificacion.php?codigo=$_dataString',
+                        size: 0.5 * bodyHeight,
+                      ),
+                    ),
             ),
           ),
         ],
